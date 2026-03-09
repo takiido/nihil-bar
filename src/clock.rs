1b22d9f0fa777c00aa6e114a39e294e5cf90c317
takiid0 <takiid0@proton.me>
2026-03-09 12:52:16 -0500

    Created clock widget

---
+++ src/clock.rs
@@ -0,0 +1,38 @@
+use gtk4::{glib, Box, Label, Orientation};
+use gtk4::prelude::WidgetExt;
+
+pub fn create_widget() -> Box{
+    let container = Box::new(Orientation::Horizontal, 0);
+    let label  = Label::new(None);
+    label.set_parent(&container);
+    schedule_clock(label);
+
+    container
+}
+
+fn update_clock(label: &Label) {
+    let now = glib::DateTime::now_local().unwrap();
+    let text = now.format("%-I:%M").unwrap();
+    label.set_text(text.as_str());
+}
+
+fn schedule_clock(label: Label) {
+    let now = glib::DateTime::now_local().unwrap();
+
+    let secs_remaining = 59 - now.second() as u64;
+    let us_remaining = 1_000_000 - now.microsecond() as u64;
+    let ms_until_next_minute = secs_remaining * 1000 + us_remaining / 1000;
+
+    update_clock(&label);
+
+    glib::timeout_add_local_once(
+        std::time::Duration::from_millis(ms_until_next_minute),
+        move || {
+            update_clock(&label);
+            glib::timeout_add_seconds_local(60, move || {
+                update_clock(&label);
+                glib::ControlFlow::Continue
+            });
+        }
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,41 @@
+mod clock;
+
 use gtk4::prelude::*;
-use gtk4::{Application, ApplicationWindow, Label};
+use gtk4::{Application, ApplicationWindow};
 use gtk4_layer_shell::{Layer, LayerShell, Edge};
-use gtk4::gdk::Display;
-use gtk4::{CssProvider, STYLE_PROVIDER_PRIORITY_APPLICATION};
 
 fn main() {
     let app = Application::builder()
         .application_id("com.example.layer-shell-app")
         .build();
 
-    app.connect_activate(build_ui);
+    app.connect_activate(create_window);
     app.run();
 }
 
-fn build_ui(app: &Application) {
+fn create_window(app: &Application) {
     let window = ApplicationWindow::builder()
         .application(app)
         .build();
 
-    let provider = CssProvider::new();
-    provider.load_from_data(
-        "
-        window.my-shell-bar {
-            background-color: rgba(0, 0, 0, 0.5);
-        }
-        
-        /* This ensures the internal drawing area is also transparent */
-        window.my-shell-bar > contents {
-            background: transparent;
-        }
-        "
-    );
-
-    gtk4::style_context_add_provider_for_display(
-        &Display::default().expect("Could not connect to a display."),
-        &provider,
-        STYLE_PROVIDER_PRIORITY_APPLICATION,
-    );
-
-    window.add_css_class("my-shell-bar");
     window.init_layer_shell();
     window.set_namespace(Some("necroshell"));
-    
     window.set_layer(Layer::Overlay);
-
     window.set_anchor(Edge::Top, true);
     window.set_anchor(Edge::Left, true);
     window.set_anchor(Edge::Right, true);
-
     window.auto_exclusive_zone_enable();
-    
-    let label = Label::new(Some("𝖓𝖎𝖍𝖎𝖑"));
-    label.set_margin_top(10);
-    label.set_margin_bottom(10);
-    
-    window.set_child(Some(&label));
+
+    add_widgets(&window);
     
     window.present();
+}
+
+fn add_widgets(window: &ApplicationWindow) {
+    let wrapper = gtk4::Box::new(gtk4::Orientation::Horizontal, 0);
+
+    let clock = clock::create_widget();
+    clock.set_parent(&wrapper);
+
+    window.set_child(Some(&wrapper));
 }
